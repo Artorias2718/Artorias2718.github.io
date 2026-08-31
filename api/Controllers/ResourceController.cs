87c0807b5c3e012616a437b4de9341fea5d4f156
Artorias2718 <arturoordonez2718@gmail.com>
2026-08-31 13:42:05 -0600

    Added Rate Limiting to API Project

--- api/Controllers/ResourceController.cs
+++ api/Controllers/ResourceController.cs
@@ -1,11 +1,13 @@
 using api.Domain.Public.Resource;
 using api.Dtos.Public;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[EnableRateLimiting("readPolicy")]
 public class ResourceController(IResourceDomainGet domainGet) : ControllerBase
 {
     [HttpGet("[action]")]
