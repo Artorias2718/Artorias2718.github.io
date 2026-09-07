44c20a34c725953b184eb8dd31b58b54f7a06102
Artorias2718 <arturoordonez2718@gmail.com>
2026-09-07 11:38:45 -0600

    Corrected syntax errors with iconMap

--- view/public/iconMap.ts
+++ view/public/iconMap.ts
@@ -1,9 +1,11 @@
 import type { ComponentType } from "react";
+import type { SvgIconTypeMap } from "@mui/material";
+import type { OverridableComponent } from "@mui/material/OverridableComponent";
 import { BookOpen, Calculator, ExternalLink, Globe2, Heart, HelpCircle, Layers, Map, MessageCircle, Users } from "lucide-react";
 import { SiDiscord, SiFacebook, SiReddit, SiYoutube } from "react-icons/si";
 import { ShowChart, TravelExplore } from "@mui/icons-material";
 
-export const iconMap: Record<string, ComponentType<{ size?: number }>> = {
+export const iconMap: Record<string, ComponentType<{ size?: number }> | OverridableComponent<SvgIconTypeMap<{}, "svg">>> = {
     BookOpen,
     Calculator,
     ExternalLink,
