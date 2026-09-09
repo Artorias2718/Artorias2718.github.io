662bca0a0001a4f5c8e1da85b9e99edad9d4c2a9
Artorias2718 <arturoordonez2718@gmail.com>
2026-09-09 00:09:30 -0600

    Fixed a glitch between the Prerender Hook and React Query

--- view/src/hooks/prerender/usePrerenderSignal.ts
+++ view/src/hooks/prerender/usePrerenderSignal.ts
@@ -1,8 +1,16 @@
 import { useEffect } from "react";
 import { useIsFetching } from "@tanstack/react-query";
+import useGetFAQs from "@/api/queryHooks/FAQ/useGetFAQs.ts";
+import useGetAboutDetails from "@/api/queryHooks/About/useGetAboutDetails.ts";
 
 function usePrerenderSignal() {
+    const aboutDetailsIcons = useGetAboutDetails(true);
+    const aboutDetailsNoIcons = useGetAboutDetails(false);
+    const faq = useGetFAQs();
+    const queries = [aboutDetailsIcons, aboutDetailsNoIcons, faq];
+
     const fetching = useIsFetching();
+    const ready = queries.every(q => q.isSuccess || q.isError);
     useEffect(() => {
         if (fetching === 0) {
             const id = requestAnimationFrame(() => {
@@ -12,6 +20,12 @@ function usePrerenderSignal() {
         }
         delete document.documentElement.dataset.prerenderReady;
     }, [fetching]);
+
+    useEffect(() => {
+        if(ready) {
+            document.documentElement.dataset.prerenderReady = 'true';
+        }
+    }, [ready]);
 };
 
 export { usePrerenderSignal };
